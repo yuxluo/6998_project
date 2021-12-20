7efcc294c0663ce72c8041afab0201f3622b2d4d
Yuxuan Luo <yxluo@umich.edu>
2021-12-19 19:44:03 -0500

    put user info lambda

--- src/lambdas/put_user_info.py
+++ src/lambdas/put_user_info.py
@@ -11,15 +11,21 @@ db_name = "MATCHING_POOL"
 
 def lambda_handler(event, context):
     print(event)
-    email = (event["email"])
-    print(email)    
+
+
     print("--------------")
     
-
-        
+    uni_ = event["uni"]
+    email_ = event["email"]
+    online_ = event["online"]
+    first_name_ = event["firstname"] 
+    last_name_ = event["lastname"]
+    skills_ = event["skill"]       
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     
+
+
     try:
         conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
     except pymysql.MySQLError as e:
@@ -31,7 +37,9 @@ def lambda_handler(event, context):
     
 
     with conn.cursor() as cur:
-        cur.execute('SELECT * FROM USERTABLE WHERE email="{}"'.format(email))
+        cur.execute('DELETE FROM USERTABLE WHERE email="{}"'.format(email_))
+        cur.execute('INSERT INTO USERTABLE (uni, email, gender, firstname, lastname, skill) values("{}", "{}", "{}", "{}", "{}", "{}")'.format(uni_, email_, online_, first_name_, last_name_, skills_))
+
         for row in cur:
             print(row)
             return json.dumps(row).replace('\"', '')
