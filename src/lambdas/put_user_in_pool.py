7cab71a04d456dfc6a3a62d0034450d80e7a9567
Yuxuan Luo <yxluo@umich.edu>
2021-12-19 23:17:53 -0500

    put user into pool lambda

--- src/lambdas/put_user_in_pool.py
+++ src/lambdas/put_user_in_pool.py
@@ -15,30 +15,25 @@ def lambda_handler(event, context):
 
     print("--------------")
     
-    uni_ = event["uni"]
+    course_id_ = event["course_id"]
     email_ = event["email"]
-    online_ = event["online"]
-    first_name_ = event["firstname"] 
-    last_name_ = event["lastname"]
-    skills_ = event["skill"]       
+
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     
-
-
     try:
         conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
     except pymysql.MySQLError as e:
         logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
         logger.error(e)
         return json.dumps("db connection failed") 
+        sys.exit()
     
     logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
     
 
     with conn.cursor() as cur:
-        cur.execute('DELETE FROM USERTABLE WHERE email="{}"'.format(email_))
-        cur.execute('INSERT INTO USERTABLE (uni, email, gender, firstname, lastname, skill) values("{}", "{}", "{}", "{}", "{}", "{}")'.format(uni_, email_, online_, first_name_, last_name_, skills_))
+        cur.execute('INSERT INTO POOLTABLE (user_email, course_id) values("{}", "{}")'.format(email_, course_id_))
         conn.commit()
         for row in cur:
             print(row)
